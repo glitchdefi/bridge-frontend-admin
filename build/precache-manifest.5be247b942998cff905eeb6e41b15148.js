d69f3f027d52581588fb48b372c5ba124ed1cdbe
Ming Nguyen <ming@glitch.finance>
2022-10-05 17:19:15 +0700

    Update build folder

---
+++ build/precache-manifest.5be247b942998cff905eeb6e41b15148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa4f05050126e0b4520c9e985fdc244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e4b17c0639cd4228518",
+    "url": "/static/css/2.b650302e.chunk.css"
+  },
+  {
+    "revision": "1e4b17c0639cd4228518",
+    "url": "/static/js/2.ccfbe3ad.chunk.js"
+  },
+  {
+    "revision": "78cff724a5d5fd96b12227aba7a74648",
+    "url": "/static/js/2.ccfbe3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babb752f4d3a003f9b3f",
+    "url": "/static/js/main.8e541817.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.8e541817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7060bfe797441361e168",
+    "url": "/static/js/runtime-main.c0317c0d.js"
+  }
+]);
